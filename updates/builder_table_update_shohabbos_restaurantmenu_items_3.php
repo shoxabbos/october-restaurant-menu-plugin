444e6f0e0602862cd633466bee3b2c6679ca01ff
Shoxabbos <19941001@inbox.ru>
2018-09-12 19:13:37 +0500

    Added new field to items table "mini_desc"

---
+++ updates/builder_table_update_shohabbos_restaurantmenu_items_3.php
@@ -0,0 +1,27 @@
+<?php namespace Shohabbos\RestaurantMenu\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateShohabbosRestaurantmenuItems3 extends Migration
+{
+    public function up()
+    {
+        Schema::table('shohabbos_restaurantmenu_items', function($table)
+        {
+            $table->string('mini_desc', 255)->nullable();
+            $table->text('desc')->nullable()->change();
+            $table->integer('price')->nullable()->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('shohabbos_restaurantmenu_items', function($table)
+        {
+            $table->dropColumn('mini_desc');
+            $table->text('desc')->nullable(false)->change();
+            $table->integer('price')->nullable(false)->change();
+        });
+    }
+}
