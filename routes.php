3c22a7e91a2b81d58c43eb85be9e4a9b3f4a538a
Shoxabbos <19941001@inbox.ru>
2018-09-12 18:40:05 +0500

    Update namespace

--- routes.php
+++ routes.php
@@ -1,4 +1,4 @@
 <?php
 Route::group(['prefix' => 'api/v1'], function () {
-   Route::resource('categories', 'shohabbos\RestaurantMenu\Controllers\Api\CategoryController');
+   Route::resource('categories', 'Shohabbos\RestaurantMenu\Controllers\Api\CategoryController');
 });
