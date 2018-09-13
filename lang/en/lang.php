00cb7de94e813771092bf9a3c146bc356fe751d0
Shoxabbos <19941001@inbox.ru>
2018-09-13 12:55:16 +0500

    To be added en translate file

---
+++ lang/en/lang.php
@@ -0,0 +1,23 @@
+<?php return [
+    'plugin' => [
+        'name' => 'Restaurant menu',
+        'description' => 'Restaurant menu',
+    ],
+    'category' => [
+        'title' => 'Title',
+        'desc' => 'Description',
+        'photo' => 'Photo',
+        'manage_category' => 'Manage category',
+        'backend_menu_lable' => 'Categories',
+    ],
+    'item' => [
+        'title' => 'Title',
+        'photos' => 'Photos',
+        'category_id' => 'Category ID',
+        'desc' => 'Description',
+        'manage_item' => 'Manage items',
+        'backend_menu_label' => 'Items',
+        'price' => 'Price',
+        'mini_desc' => 'Mini description',
+    ],
+];
