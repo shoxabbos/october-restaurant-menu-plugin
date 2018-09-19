ce16037b6c34c3d05687a83b7a697b84f582ef4d
Shoxabbos <19941001@inbox.ru>
2018-09-19 13:24:57 +0500

    Bug fix

--- helpers/Helpers.php
+++ helpers/Helpers.php
@@ -8,9 +8,10 @@ Class Helpers {
 			'status_code' => (isset($statusCode)) ? $statusCode : 500,
 			'message' => (isset($message)) ? $message : 'error'
 		];
-		if (count($data) > 0) {
+		
+		//if (is_array($data) && count($data) > 0) {
 			$arr['data'] = $data;
-		}
+		//}
 
 		return response()->json($arr, $arr['status_code']);
 		//return $arr;
